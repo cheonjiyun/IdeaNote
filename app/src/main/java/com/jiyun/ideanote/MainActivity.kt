1f405024f85d907d95ce447998d3386fd9efd672
gunbambam <gunbam@gmail.com>
2023-01-25 17:42:29 +0900

    save memo - file - internal storage

--- app/src/main/java/com/jiyun/ideanote/MainActivity.kt
+++ app/src/main/java/com/jiyun/ideanote/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +18,12 @@ class MainActivity : AppCompatActivity() {
 
             val memo = findViewById<EditText>(R.id.memo)
             Toast.makeText(this@MainActivity, memo.text.toString(), Toast.LENGTH_LONG).show()
+
+            //메모 파일 저장
+            val outputFile : FileOutputStream = openFileOutput("fileName", MODE_PRIVATE)
+            outputFile.write(memo.text.toString().toByteArray()) // memo: String DATA
+            outputFile.flush()
+            outputFile.close()
 
         }
     }
